666262a0bfd5b70208ecbc3c1c436771287ce445
Aman Vijay <amanvijay.cse19@jecrc.ac.in>
2018-07-05 20:31:00 +0530

    Stemming of words

---
+++ stemmer.py
@@ -0,0 +1,11 @@
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+ps=PorterStemmer()
+example_words=["python","pythoner","pythoning","pythoned","pythonly"]
+for w in example_words:
+    print(ps.stem(w))
+
+new_text="It is very important to be pythonly while ypu are pythoning with python.All pythoners loves python"
+words=word_tokenize(new_text)
+for word in words:
+    print(ps.stem(word))
