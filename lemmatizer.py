024842914c14c41cbf7c7d2ce5f7cd787ce23633
Aman Vijay <amanvijay.cse19@jecrc.ac.in>
2018-07-05 23:00:26 +0530

    lemmatizer

---
+++ lemmatizer.py
@@ -0,0 +1,5 @@
+from nltk.stem import WordNetLemmatizer
+lemmatizer=WordNetLemmatizer()
+print(lemmatizer.lemmatize("better","a"))
+print(lemmatizer.lemmatize("good","a"))
+print(lemmatizer.lemmatize("great","a"))
